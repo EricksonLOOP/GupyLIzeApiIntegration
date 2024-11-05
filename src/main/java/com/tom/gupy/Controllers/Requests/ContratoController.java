caa49955fa25395cb662dee3d80a8b8be5bea5b4
Erickson Dias <103853894+EricksonLOOP@users.noreply.github.com>
2024-11-05 14:02:23 -0300

    Feat: HelloWorld endpoit

--- src/main/java/com/tom/gupy/Controllers/Requests/ContratoController.java
+++ src/main/java/com/tom/gupy/Controllers/Requests/ContratoController.java
@@ -1,9 +1,14 @@
 package com.tom.gupy.Controllers.Requests;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
 public class ContratoController {
+    @GetMapping("/")
+    public String HelloWorld(){
+        return "Hello World";
+    }
 }
